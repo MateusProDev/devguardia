abb6a58660e0b4399dcb507dc638bb1cee47955e
mateusprodev <mateusferreiraprodev@gmail.com>
2026-04-12 17:19:20 -0300

    fix: add UsersModule + FirebaseAdminService to AuthModule

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
+import { FirebaseAdminService } from './firebase-admin.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController],
+  providers: [FirebaseAdminService],
+  exports: [FirebaseAdminService],
 })
 export class AuthModule {}
