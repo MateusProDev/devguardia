0095178b196a55137c055174503e2ba036a7b035
mateusprodev <mateusferreiraprodev@gmail.com>
2026-05-01 09:15:15 -0300

    temp: remove LocalCacheService to unblock deploy

--- backend/src/common/guards/guards.module.ts
+++ backend/src/common/guards/guards.module.ts
@@ -2,12 +2,11 @@ import { Global, Module } from '@nestjs/common';
 import { FirebaseAuthGuard } from './firebase-auth.guard';
 import { FirebaseAdminService } from '../../modules/auth/firebase-admin.service';
 import { UsersModule } from '../../modules/users/users.module';
-import { LocalCacheService } from '../cache/local-cache.service';
 
 @Global()
 @Module({
   imports: [UsersModule],
-  providers: [FirebaseAdminService, FirebaseAuthGuard, LocalCacheService],
-  exports: [FirebaseAdminService, FirebaseAuthGuard, LocalCacheService],
+  providers: [FirebaseAdminService, FirebaseAuthGuard],
+  exports: [FirebaseAdminService, FirebaseAuthGuard],
 })
 export class GuardsModule {}
